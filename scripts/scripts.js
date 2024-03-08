1af9fbd329238f456b8132450f5ad15430036470
Jayshan <jixyjix@gmail.com>
2024-03-09 00:21:05 +0900

    Added more onclick funcitonality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,27 @@
 function AlertMessage()
 {
     alert("Random message");
+    document.getElementById("p1").innerText = "The alert popped up";
+}
+
+function ChangeColourBlock(color)
+{
+    document.getElementById("clrblock").style.backgroundColor = color;
+}
+
+function ChangeCase(lettercase)
+{
+    let text = document.getElementById("p1").innerText;
+    if (lettercase == "upper")
+    {
+        document.getElementById("p1").innerText = text.toUpperCase();
+    }
+    else if (lettercase == "lower")
+    {
+        document.getElementById("p1").innerText = text.toLowerCase();
+    }
+    else
+    {
+        document.getElementById("p1").innerText = "The case entered was wrong";
+    }
 }
