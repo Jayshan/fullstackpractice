362e89302bf15a38892afe1724004a4d8ab25e97
Jayshan <jixyjix@gmail.com>
2024-04-02 09:49:50 +0900

    Added some extra JS functions

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,13 @@
-function AlertMessage()
-{
+function AlertMessage() {
     alert("Random message");
     document.getElementById("p1").innerText = "The alert popped up";
 }
 
-function ChangeColourBlock(color)
-{
+function ChangeColourBlock(color) {
     document.getElementById("clrblock").style.backgroundColor = color;
 }
 
-function ChangeCase(lettercase)
-{
+function ChangeCase(lettercase) {
     let text = document.getElementById("p1").innerText;
     if (lettercase == "upper")
     {
@@ -29,8 +26,7 @@ function ChangeCase(lettercase)
 //Seems like the array find() function always takes in a function with default built in parameters
 //That's why you don't have to pass any parameters when calling myFunction
 //value is the only required parameter, index and array are optional
-function ArrayFind()
-{
+function ArrayFind() {
     const numbers = [4, 9, 16, 25, 29];
             let first = numbers.find(myFunction);
     
@@ -41,3 +37,19 @@ function ArrayFind()
             }
 
 }
+
+function OpenWindow() {
+    window.open("https://www.w3schools.com");
+    //window.location.assign("https://www.w3schools.com")
+}
+
+//code to access browser geolocation doesn't seem to work on Mac without some kind of workaround
+//because the local dev site is not secure i.e. https 
+// const x = document.getElementById("geolocation");
+// function GeoLocate() {
+//     navigator.geolocation.getCurrentPosition(ShowPosition);
+// }
+
+// function ShowPosition(position) {
+//     x.innerHTML = "Latitude: " + position.coords.latitude;
+// }
